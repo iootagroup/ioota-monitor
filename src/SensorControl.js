38a3c8cd7487b29cd4f6ec3c486f09b68065aa2f
KimiNyG <kimi.nygren@student.lamk.fi>
2019-03-04 16:29:00 +0200

    Some testing

--- src/SensorControl.js
+++ src/SensorControl.js
@@ -1,11 +1,34 @@
+var GrovePi = require('node-grovepi').GrovePi
+
+var Commands = GrovePi.commands
+var Board = GrovePi.board
+
+var chainableRGBLedDigitalSensor = GrovePi.sensors.chainableRGBLedDigitalSensor
+
+var board = new Board({
+    debug: true,
+    onError: function(err) {
+      console.log('Something wrong just happened')
+      console.log(err)
+    },
+    onInit: function(res) {
+      if (res) {
+        console.log('GrovePi Version :: ' + board.version())
+
+        var RGBLed = new chainableRGBLedDigitalSensor(0)
+        console.log('RGB led Sensor (start watch)')
+        RGBLed.on('change', function(res) {
+          console.log('RGB led onChange value=' + res)
+        })
+        RGBLed.watch()
+      }
+    }
+  })
+
+  board.init();
 
 
+//module.exports = {
+//    functionName: ChainableRGBLedDigitalSensor
 
-
-
-
-
-module.exports = {
-    functionName: functionName,
-
-}
+//}
