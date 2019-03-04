d346965b9f32da35e8378d9190d75866dd17dba1
pkupias <petri.kupias@student.lamk.fi>
2019-03-04 14:31:24 +0200

    Read AirQualitySensor and write value to console

--- src/GrovePiReader.js
+++ src/GrovePiReader.js
@@ -1,3 +1,27 @@
 var GrovePI = require('node-grovepi').GrovePI;
 var Commands = GrovePI.commands;
 var Board = GrovePI.Board;
+
+var AirQualityAnalogSensor = GrovePi.sensors.AirQualityAnalog;
+
+var board = new Board({
+  debug: true,
+  onError: function(err) {
+    console.log('Something wrong just happened')
+    console.log(err)
+  },
+  onInit: function(res) {
+    if (res) {
+      console.log('GrovePi Version :: ' + board.version());
+
+      var airQualitySensor = new AirQualityAnalogSensor(0);
+      console.log('Air Quality Analog Sensor (start watch)');
+      airQualitySensor.on('change', function(res) {
+        console.log('Air Quality onChange value=' + res)
+      });
+      airQualitySensor.watch();
+    }
+  }
+});
+
+board.init();
