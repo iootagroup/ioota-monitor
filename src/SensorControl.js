18d8d217fd13db306451c5c241180431fb3c9f3c
KimiNyG <kimi.nygren@student.lamk.fi>
2019-03-05 12:50:57 +0200

    testin rgb

--- src/SensorControl.js
+++ src/SensorControl.js
@@ -15,19 +15,20 @@ var board = new Board({
       if (res) {
         console.log('GrovePi Version :: ' + board.version());
 
-        var RGBLed = new ChainableRGBLedDigitalSensor(8,1);
-        RGBLed.init()
+        var RGBLed = new ChainableRGBLedDigitalSensor(8, 1);
+        
         //var RGBLedcolor = RGBLed.Commands.storeColor(0,255,0);
         //RGBLed.Commands.chainbleRgbLedSetModulo(RGBLedcolor);
         //var RGBLedcolor = Commands.storeColor(0,255,0);
         //RGBLed.Commands.chainbleRgbLedSetModulo(RGBLedcolor);
 
         console.log('RGB led Sensor (start watch)');
-        RGBLed.off('change', function(res) {
+        RGBLed.on('change', function(res) {
+
           console.log('RGB led onChange value=' + res)
         });
         
-        RGBLed.watch();
+        RGBLed.watch(1000);
       }
     }
   })
