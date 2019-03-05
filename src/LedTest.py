f22f7e89ab1e245b01513195fd92628221ffad60
KimiNyG <kimi.nygren@student.lamk.fi>
2019-03-05 11:18:38 +0200

    testing led with python

--- src/LedTest.py
+++ src/LedTest.py
@@ -1,50 +1,3 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- @KimiNyG Sign out
-64
-340 357 DexterInd/GrovePi
- Code  Issues 42  Pull requests 3  Projects 1  Wiki  Insights
-GrovePi/Software/Python/grove_chainable_rgb_led/grove_chainable_rgb_led_test.py
- Dexter Industries Update CRLF Endings
-1fc3157  on Mar 24, 2017
-175 lines (125 sloc)  5 KB
-    
-#!/usr/bin/env python
-#
-# GrovePi Example for using the Grove Chainable RGB LED (http://www.seeedstudio.com/wiki/Grove_-_Chainable_RGB_LED)
-#
-# The GrovePi connects the Raspberry Pi and Grove sensors.  You can learn more about GrovePi here:  http://www.dexterindustries.com/GrovePi
-#
-# Have a question about this example?  Ask on the forums here:  http://www.dexterindustries.com/forum/?forum=grovepi
-#
-'''
-## License
-The MIT License (MIT)
-GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
-Copyright (C) 2017  Dexter Industries
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-'''
-
 import time
 import grovepi
 
@@ -184,15 +137,4 @@ except KeyboardInterrupt:
     grovepi.chainableRgbLed_test(pin, numleds, testColorBlack)
 except IOError:
     print ("Error")
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
