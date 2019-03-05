5ccd84f2a50d14ee2aeeb1eac976fe6baba4090a
Miikka Saxlund <miikka.saxlund@outlook.com>
2019-03-05 10:04:53 +0200

    Modulized the client configs & classes

--- src/RestClient.js
+++ src/RestClient.js
@@ -1,32 +1,23 @@
 const request = require('request')
 const fs = require('fs')
+const rest = require('./RestClient.config.js')
+const { Query } = require('./Query.js')
 
-// Define the hostname
-let hostname = 'http://health-safety.dev.api.kemppi.com'
-
-// Define the port
-let port = 8080
-
-// Define the path
-let path = '/api/sensordata?type=SENSORNAME&start=2010-01-01T00:00:00Z&end=2019-04-03T18:12:41Z&limit=100&from=0'
-
-// Define the url
-var url = hostname + ':' + port + path
-
-// Define the JSON web token
-let jwt = fs.readFileSync('./group4.jwt')
-
-// Define the options
-var options = {
-    url: url,
-    headers: {
-        'Authorization': 'Bearer ' + jwt
-    }
+//let qfile = fs.readFileSync()
+let qConfig = {
+    sensor: 'sensorname',
+    tsStart: new Date(2018, 11, 24, 10, 33, 30),
+    tsEnd: new Date(2018, 11, 24, 18, 13, 12),
+    limit: 100,
+    from: 0
 }
+
+let q = new Query(qConfig)
+q.path = q.returnPath()
 
 module.exports = {
     getStatus: (callback) => {
-        request.get(`${hostname}:${port}/health`, (err, res, body) => {
+        request.get(rest.url + rest.host.paths.status, (err, res, body) => {
             if (err) throw err
             console.log('Received response')
             status = JSON.parse(body).status == 'ONLINE' ? "Online" : "Offline"
@@ -35,20 +26,23 @@ module.exports = {
         })
     },
     getData: (callback) => {
-        request.get(options, (err, res, body) => {
+        //request.get({url: rest.options.url + query.path, headers: {'Authorization': 'Bearer ' + jwt}}, (err, res, body) => {
+        request.get({ url: rest.url + q.path, headers: rest.headers }, (err, res, body) => {
             let data = body 
             // Parse the data here
             return callback(data)
         })
     },
     postData: (data) => {
-        request.post({url: 'http://health-safety.dev.api.kemppi.com:8080/api/sensordata', headers: {'Authorization': 'Bearer ' + jwt }, json: data}, (err, res, body) => {
+        //request.post({url: 'http://health-safety.dev.api.kemppi.com:8080/api/sensordata', headers: {'Authorization': 'Bearer ' + jwt }, json: data}, (err, res, body) => {
+        request.post({ url: rest.url + rest.host.paths.sensordata, headers: rest.headers, json: data }, (err, res,body) => {
             if (err) throw err
             console.log(body)
         })
     }, 
     deleteData: (callback) => {
-        request.delete({url: 'http://health-safety.dev.api.kemppi.com:8080/api/sensordata', headers: {'Authorization': 'Bearer ' + jwt }, json: {clear: true}}, (err, res, body) => {
+        //request.delete({url: 'http://health-safety.dev.api.kemppi.com:8080/api/sensordata', headers: {'Authorization': 'Bearer ' + jwt }, json: {clear: true}}, (err, res, body) => {
+        request.delete({ url: rest.url + rest.host.paths.sensordata, headers: rest.headers, json: { clear: true } }, (err, res, body) => {
             if (err) throw err
             return callback(body.message)
             console.log(body.message)
